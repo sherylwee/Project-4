95a0fb4c14d07e217031bb6483b790da50d915c2
sherylwee <wee_mantheng@hotmail.com>
2019-02-25 16:56:27 +0800

    fetched data for companies

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -2,14 +2,47 @@ import React from 'react';
 import Newitems from './newitems'
 
 import { Route, Link } from "react-router-dom";
+import 'whatwg-fetch'
 
-class App extends React.Component{
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      company: [],
+    };
+  }
+
+  componentDidMount() {
+
+    var reactThis = this;
+
+    fetch('http://localhost:3000/companies.json')
+      .then(function (response) {
+        return response.json()
+      }).then(function (json) {
+        console.log({json})
+        reactThis.setState({company: json})
+      }).catch(function (err) {
+        console.log(err)
+      })
+  }
+
 
   render() {
-    return(
+
+    const names = this.state.company.map((test,index)=>{
+      return <div key={index}>
+                <h1>{test.name}</h1>
+                <p>{test.description}</p>
+                <p>{test.contact}</p>
+             </div>
+    })
+    
+    return (
       <div>
         <nav>
-          <h1>React Router</h1>
+          
+          {names}
           <Link to="/newitems">New items</Link>
           <Link to="/useraccept">Users to accept</Link>
         </nav>
@@ -17,13 +50,13 @@ class App extends React.Component{
           <Route
             path='/newitems'
             render={() => (
-              <Newitems/>
+              <Newitems />
             )}
           />
           <Route
             path='/useraccept'
             render={() => (
-              <Oranges/>
+              <Oranges />
             )}
           />
         </main>
